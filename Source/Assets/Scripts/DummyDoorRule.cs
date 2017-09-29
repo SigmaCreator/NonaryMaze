cc367a4ab3cd1a5e545c9c5bf36aa64bda552d5a
andre <andre.antonitsch@acad.pucrs.br>
2017-09-28 23:12:47 -0300

    Basic project structure

---
+++ Source/Assets/Scripts/DummyDoorRule.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO Change this architecture to an abstract class.
+// Is the that scriptableobject inheritance required?
+public class DummyDoorRule : ScriptableObject, IDoorOpeningRule {
+
+
+}
