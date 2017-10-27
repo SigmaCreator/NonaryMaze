1df0d2cec3f58b9351e571875b584c1db8acbef3
andreantonitsch <andre.antonitsch@acad.pucrs.br>
2017-10-27 10:40:07 -0200

    PlayerState defined. Players now have well defined discrete states.

--- Source/Assets/Scripts/Player.cs
+++ Source/Assets/Scripts/Player.cs
@@ -8,10 +8,14 @@ public class Player : MonoBehaviour {
     // Set up a state machine for character states.
     // Selected/unselected
 
+    public enum PlayerState{
+    UNSELECTED,
+    SELECTED
+
+    };
+
     [SerializeField]
     int playerCode = 0;
-
- 
     public int Code
     {
         get { return playerCode; }
@@ -20,36 +24,37 @@ public class Player : MonoBehaviour {
 
     public Sprite[] Sprites
     {
-        get
-        {
-            return _sprites;
-        }
-
-        set
-        {
-            _sprites = value;
-        }
+        get { return _sprites; }
+        set { _sprites = value; }
     }
 
+    [SerializeField]
+    PlayerState _state = PlayerState.UNSELECTED;
+    public PlayerState State {
+        get { return _state; }
+    }
+
+    // cached reference to the gamecontroller. useful?
     GameController gc;
 
     [SerializeField]
     Sprite[] _sprites;
 
+    // cached references to the sprite components. Controls the sprite character and the numbered badge.
     SpriteRenderer _chracterSprite;
-
     SpriteRenderer _codeSprite;
 
     Collider2D _collider;
 
     Color[] colors;
     Vector3[] scales;
-    bool _selected = false;
-
-    float _selectionAnimator = Mathf.PI/2f;
-
+    
     void Start()
     {
+        // Sets up all sprites and states.
+        // Caches necessary component references.
+        // Initializes the procedural components.
+
         _chracterSprite = transform.Find("CharacterSprite").gameObject.AddComponent<SpriteRenderer>();
         _codeSprite = transform.Find("CodeSprite").gameObject.AddComponent<SpriteRenderer>();
 
@@ -71,34 +76,56 @@ public class Player : MonoBehaviour {
 
     public void Update()
     {
-        if (_selected) {
-
-            _selectionAnimator += Time.deltaTime;
-
-            //TODO ineffective, cache this cleanly
-            _chracterSprite.gameObject.transform.Rotate(new Vector3(0f, 0f, Mathf.Sin(_selectionAnimator)));
-            
+        switch (_state)
+        {
+            case (PlayerState.SELECTED):
+                //TODO ineffective, cache this cleanly
+                _chracterSprite.gameObject.transform.Rotate(new Vector3(0f, 0f, (Mathf.Sin(Time.time * 4) / 2)));
+                //_chracterSprite.gameObject.transform.Translate(new Vector3(Mathf.Cos(Time.time * 4) / 100, Mathf.Sin(Time.time * 4) / 100, 0f));
+                break;
+            default:
+                break;
+                
         }
+
+    }
+
+    public void ChangeState(Player.PlayerState state) {
+        //Switches into new PlayerState state
+    
+        _state = state;
+        switch (_state)
+        {
+            case (PlayerState.SELECTED):
+                _chracterSprite.sprite = Sprites[1];
+                transform.localScale = scales[1];
+                break;
+
+            case (PlayerState.UNSELECTED):
+                _chracterSprite.transform.rotation = Quaternion.identity;
+                //_chracterSprite.color = colors[0];
+                _chracterSprite.sprite = Sprites[0];
+                    transform.localScale = scales[0];
+                break;
+            default:
+                break;
+                
+        }
+        
     }
 
     public void OnTouch() {
-        
-        if (_selected)
-        {
-            _selected = !_selected;
-            //_chracterSprite.color = colors[0];
-            _chracterSprite.sprite = Sprites[0];
-            transform.localScale = scales[0];
 
-            _selectionAnimator = Mathf.PI / 2f;
-            _chracterSprite.transform.rotation = Quaternion.identity;
-        }
-        else
+        switch (_state)
         {
-            _selected = !_selected;
-            //_chracterSprite.color = colors[1];
-            _chracterSprite.sprite = Sprites[1];
-            transform.localScale = scales[1];
+            case (PlayerState.SELECTED):
+                ChangeState(PlayerState.UNSELECTED);
+                break;
+            case (PlayerState.UNSELECTED):
+                ChangeState(PlayerState.SELECTED);
+                break;
+            default:
+                break;
         }
 
     }
